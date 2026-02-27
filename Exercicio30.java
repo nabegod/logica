98fadeb4501c37908ee94d64c3603fff7e125cbf
instrutor-edmar <ssg.s24ultra@gmail.com>
2026-02-27 20:37:39 +0000

    Resolução da questão 30

--- Exercicio30.java
+++ Exercicio30.java
@@ -3,10 +3,18 @@
 import java.util.Scanner;
 
 public class Exercicio30 {
+	    public static int fatorial (int numero){
+        if(numero == 1) return 1;
+        return numero*fatorial(numero-1);
+    }
+    
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
 
-		//escreva seu código aqui
+		System.out.print("Informe um número: ");
+		int num = input.nextInt();
+		
+		System.out.println("O fatorial de " + num + "é: " +fatorial(num));
 		
 		input.close();
 	}
